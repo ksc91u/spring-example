a7f6e834417034a3e0c24bcf5997590fff21ff90
ksc91u <andreaskung@17.media>
2020-03-06 19:02:04 +0800

    fix build, listen to any address

--- src/main/kotlin/com/ksc91u/youtube/YoutubeApplication.kt
+++ src/main/kotlin/com/ksc91u/youtube/YoutubeApplication.kt
@@ -1,11 +1,7 @@
 package com.ksc91u.youtube
 
-import com.apple.eawt.Application
-import com.ksc91u.youtube.controller.MainController
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 class YoutubeApplication
