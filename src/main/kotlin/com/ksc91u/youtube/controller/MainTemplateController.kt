bcd1c61d7776a589bc06ab71c902f01af7e7b8c7
ksc91u <ksc91u@gmail.com>
2020-03-28 00:59:39 +0800

    detect fb bot

--- src/main/kotlin/com/ksc91u/youtube/controller/MainTemplateController.kt
+++ src/main/kotlin/com/ksc91u/youtube/controller/MainTemplateController.kt
@@ -4,14 +4,17 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import javax.servlet.http.HttpServletRequest
 
 @Controller
 class MainTemplateController{
     @GetMapping("/index")
     fun index(model: Model,
+              request: HttpServletRequest,
               @RequestParam("i", required = false, defaultValue = "Wave 壞壞小妖精") userId: String,
               @RequestParam("c", required = false, defaultValue = "0") char: String): String {
 
+        val userAgentInfo = request.getHeader("User-Agent");
         val names = arrayListOf<String>("智商 180", "韓國瑜珈老師", "台灣天才IT大臣")
         val pics = arrayListOf<String>("https://img.ltn.com.tw/Upload/talk/page/800/2020/03/27/phpdbowmV.jpg",
                 "https://img.ltn.com.tw/Upload/news/600/2020/03/27/phpBvG7YQ.jpg",
@@ -22,6 +25,7 @@ class MainTemplateController{
 
         model.addAttribute("title", "$userId 在 Wave 抽到了 $name")
         model.addAttribute("character", pic)
+        model.addAttribute("fb", userAgentInfo.contains("facebook").not())
 
         return "wave"
     }
--- src/main/resources/template/wave.ftl
+++ src/main/resources/template/wave.ftl
@@ -6,7 +6,9 @@
     <meta property="og:image" content="${character}" />
     <meta property="og:description" content="快來抽 - ${title}" />
     <meta property="og:url" content="https://www.wave.com.tw/" />
+    <#if fb>
     <meta http-equiv="Refresh" content="5; url=https://wave.com.tw" />
+    </#if>
 </head>
 <body>
 
